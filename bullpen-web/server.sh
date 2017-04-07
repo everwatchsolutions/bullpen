e03e05b4677996c299dac5b3e4d6f80d1db8cddc
Dylan K <dylankolson@gmail.com>
2017-04-07 14:22:44 -0400

    Create server.sh
    file for deploying to the official server

---
+++ bullpen-web/server.sh
@@ -0,0 +1,11 @@
+cd apache-tomcat-8.5.13/webapps/ #CD into your tom cat webapps folder
+rm -f ROOT.war #remove the war file and folder
+rm -rf ROOT
+cd ..
+cd ..
+cd bullpen/ # move into ats
+mvn clean install
+cd bullpen-web/target/
+mv bullpen-web.war /opt/apache-tomcat-8.5.13/webapps/ROOT.war #move and rename the war file to root in the webapps folder
+cd /opt/apache-tomcat-8.5.13/bin/ #move into the bin folder
+sh catalina.sh run
