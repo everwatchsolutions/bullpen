6254d6d87e485efc9fbae4143cbe44aad519199a
Dylan K <dylankolson@gmail.com>
2017-04-08 17:13:14 -0400

    Update server.sh

--- bullpen-web/server.sh
+++ bullpen-web/server.sh
@@ -7,5 +7,4 @@ cd bullpen/ # move into ats
 mvn clean install
 cd bullpen-web/target/
 mv bullpen-web.war /opt/apache-tomcat-8.5.13/webapps/ROOT.war #move and rename the war file to root in the webapps folder
-cd /opt/apache-tomcat-8.5.13/bin/ #move into the bin folder
-sh catalina.sh run
+cd /opt/apache-tomcat-8.5.13/bin/ #move into the bin folde
